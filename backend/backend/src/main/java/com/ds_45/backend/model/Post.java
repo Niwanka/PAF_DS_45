e0e93bd518ea7301f0b9597b87ad66f513a0aca4
Niwanka <niwankapathirathna@gmail.com>
2025-04-19 19:59:43 +0530

    getters and setters updated in post.java

--- backend/backend/src/main/java/com/ds_45/backend/model/Post.java
+++ backend/backend/src/main/java/com/ds_45/backend/model/Post.java
@@ -20,4 +20,45 @@ public class Post {
     private List<String> likes;  
     private LocalDateTime createdAt; 
     private LocalDateTime updatedAt;  
+
+      // Getters and Setters
+      public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
