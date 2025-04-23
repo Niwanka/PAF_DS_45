e74ed075a932f80a85b4425a6d4b328f43972b2b
Yasasvi Vilochana <lightfury200231@gmail.com>
2025-04-23 10:13:27 +0530

    Create LearningProgressUpdateRepository.java

---
+++ backend/backend/src/main/java/com/ds_45/backend/repository/LearningProgressUpdateRepository.java
@@ -0,0 +1,14 @@
+package com.ds_45.backend.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ds_45.backend.model.LearningProgressUpdate;
+import java.util.List;
+
+@Repository
+public interface LearningProgressUpdateRepository extends MongoRepository<LearningProgressUpdate, String> {
+    
+    List<LearningProgressUpdate> findByUserID(String userID); //find updates by user ID
+
+}
