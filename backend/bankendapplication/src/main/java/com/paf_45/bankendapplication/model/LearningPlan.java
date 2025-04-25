75b8e4f83e29e278d4a5b1711e673ec0095b80c5
Savithma <chanuthisavi@gmail.com>
2025-04-26 02:56:20 +0530

    Refactor LearningPlan model to include createdAt, resources, and status fields

--- backend/bankendapplication/src/main/java/com/paf_45/bankendapplication/model/LearningPlan.java
+++ backend/bankendapplication/src/main/java/com/paf_45/bankendapplication/model/LearningPlan.java
@@ -1,6 +1,10 @@
 package com.paf_45.bankendapplication.model;
 
 import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 //import java.time.LocalDateTime;
@@ -16,9 +20,14 @@ public class LearningPlan {
     private String title; // The title of the learning plan
     private String description; // The detailed description of the learning plan
     private String topics; // A list of topics covered in the learning plan
+    private LocalDateTime createdAt;    // Timestamp of when the learning plan was created
     private String completionDate; // Target date for completing the learning plan
     //private LocalDateTime createdAt; // Timestamp of when the learning plan was created
     //private LocalDateTime updatedAt; // Timestamp of the last update to the learning plan
+
+    private List<String> resources;     // List of URLs
+    private String status;              // e.g., "draft", "in-progress", "completed"
+    
 
     // Additional fields 
     
