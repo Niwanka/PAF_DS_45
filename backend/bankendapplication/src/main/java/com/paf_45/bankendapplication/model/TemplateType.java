a29e24fce35b665a289c638a263c97e040197669
Yasasvi Vilochana <lightfury200231@gmail.com>
2025-04-25 07:43:10 +0530

    Create templatetype.java file

---
+++ backend/bankendapplication/src/main/java/com/paf_45/bankendapplication/model/TemplateType.java
@@ -0,0 +1,9 @@
+package com.paf_45.bankendapplication.model;
+
+public enum TemplateType {
+    COMPLETED_TUTORIAL,
+    LEARNED_NEW_SKILL,
+    TIME_INVESTED,
+    PRACTICE_SESSION,
+    JOINED_WORKSHOP
+}
